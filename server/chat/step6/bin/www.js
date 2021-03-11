816108b6cc8dd960388cf3196b5e2fcc27e98693
uzoolove@gmail.com <uzoolove@gmail.com>
2021-03-11 11:50:35 +0900

    ,

--- server/chat/step6/bin/www.js
+++ server/chat/step6/bin/www.js
@@ -8,3 +8,7 @@ var server = new http.createServer(app);
 server.listen(80, function(){
   console.log('HTTP 서버 구동.');
 });
+
+// socket.io 서버 구동
+var io = require('socket.io')(server);
+require('../chatserver')(io);
