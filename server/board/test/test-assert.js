58845b4fc3e8d41d951fe6c836b746c8353f1052
uzoolove@gmail.com <uzoolove@gmail.com>
2021-03-12 13:53:20 +0900

    .

--- server/board/test/test-assert.js
+++ server/board/test/test-assert.js
@@ -5,6 +5,7 @@ var success = 0;
 var fail = 0;
 console.log('테스트 시작.');
 
+/*
 var result = upper.sync('hello', 2);
 if(result == 'L'){
   console.log('1. 통과');
@@ -13,6 +14,60 @@ if(result == 'L'){
   console.log('1. 실패');
   fail++;
 }
+
+var result = upper.sync('hello', 5);
+if(result == null){
+  console.log('2. 통과');
+  success++;
+}else{
+  console.log('2. 실패');
+  fail++;
+}
+*/
+
+/* 독립적인 테스트를 위해서 작성해야 하는 코드가 너무 많다.
+try{
+  var result = upper.sync('hello', 2);
+  assert.strictEqual(result, 'L');
+  console.log('1. 통과');
+  success++;
+}catch(err){
+  console.log('1. 실패');
+  fail++;
+}
+try{
+  var result = upper.sync('hello', 5);
+  assert(result==null);
+  console.log('2. 통과');
+  success++;
+}catch(err){
+  console.log('2. 실패');
+  fail++;
+}
+*/
+
+/* 비동기 함수 테스트(순차적인 테스트가 필요할 경우 코드가 복잡해진다.)
+upper.async('hello', 5, function(result){
+  try{
+    assert(result==null);
+    console.log('1. 통과');
+    success++;
+  }catch(err){
+    console.log('1. 실패');
+    fail++;
+  }
+  upper.async('hello', 2, function(result){
+    try{
+      assert(result=='L');
+      console.log('2. 통과');
+      success++;
+    }catch(err){
+      console.log('2. 실패');
+      fail++;
+    }
+  });
+});
+*/
 
 console.log('테스트 종료.');
 console.log('success', success);
--- server/board/test/test-mocha.js
+++ server/board/test/test-mocha.js
@@ -4,4 +4,82 @@ var model = require('../models/board');
 
 var article = {writer: '김철수', title: 'mocha 테스트', content: '김철수 만세'};
 
+// test suite
+describe('# upper 함수 테스트', function(){
+  describe('# 동기 테스트', function(){
+    // unit test
+    it('sync(hello, 2)', function(){
+      assert(upper.sync('hello', 2) === 'L');
+    });
+    it('sync(hello, 5)', function(){
+      assert.strictEqual(upper.sync('hello', 5), null);
+    });
+    it('sync(hello, 0)', function(){
+      assert.strictEqual(upper.sync('hello', 0), 'H');
+    });
+  });
+  describe('# 비동기 테스트', function(){
+    this.timeout(5000);
+    // unit test
+    it('async(hello, 2)', function(done){
+      this.timeout(1000);
+      upper.async('hello', 2, function(result){
+        assert(result === 'L');
+        done();
+      });
+    });
+    it('async(hello, 5)', function(done){
+      upper.async('hello', 5, function(result){
+        assert(result === null);
+        done();
+      });
+    });
+    it('async(hello, 0)', function(done){
+      upper.async('hello', 0, function(result){
+        assert(result === 'H');
+        done();
+      });
+    });
+  });
+});
+
+var newNo;
+describe.only('# 게시판 테스트', function(){
+  // 사전 작업 정의
+
+  var oldList;
+  before(function(done){
+    model.list(function(result){
+      oldList = result;
+      done();
+    });
+  });
+
+  describe('등록', function(){
+    it('등록 요청', function(done){
+      model.create(article, function(no){
+        assert.strictEqual(typeof no, 'number');
+        newNo = no;
+        done();
+      });
+    });
+    it('등록한 게시물 조회', function(done){
+      model.show(newNo, function(newArticle){
+        assert.deepStrictEqual(newArticle, article);
+        done();
+      });
+    });
+  });
+  describe('삭제', function(){
+    it('삭제 요청', function(done){
+      model.remove(newNo, done);
+    });
+    it('목록 조회', function(done){
+      model.list(function(result){
+        assert.deepStrictEqual(result, oldList);
+        done();
+      });
+    });
+  });
+});
 
